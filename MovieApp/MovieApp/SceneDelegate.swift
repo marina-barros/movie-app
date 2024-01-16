e65712046f874848a77586b3dda71698ed59324b
Marina Barros <marina.barros@popcode.com.br>
2024-01-16 19:46:37 -0300

    initial project commit

---
+++ MovieApp/MovieApp/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  MovieApp
+//
+//  Created by Marina Barros on 16/01/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            // TODO: Add first ViewController
+            window.rootViewController = UINavigationController(rootViewController: ViewController())
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
+
